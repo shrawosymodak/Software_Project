35ce2271022fce99ba8c3ae3801858b333af413e
shra <modak2007035@gmail.com>
2024-05-24 20:07:34 +0600

    expensefragment updated

--- app/src/main/java/com/example/myapplication/mainfragment/expenseFragment.java
+++ app/src/main/java/com/example/myapplication/mainfragment/expenseFragment.java
@@ -1,17 +1,7 @@
 package com.example.myapplication.mainfragment;
 
-
 import android.content.DialogInterface;
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Spinner;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -21,24 +11,35 @@ import androidx.recyclerview.widget.ItemTouchHelper;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.example.myapplication.Class.BalanceClass;
-import com.example.myapplication.Class.ExpenseClass;
-import com.example.myapplication.R;
-//import com.example.myapplication.StatusActivity;
-import com.example.myapplication.adapters.expenseAdapter;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import com.example.mainprojectpersonallifetracker.Class.BalanceClass;
+import com.example.mainprojectpersonallifetracker.Class.ExpenseClass;
+import com.example.mainprojectpersonallifetracker.Class.TodoClass;
+import com.example.mainprojectpersonallifetracker.R;
+import com.example.mainprojectpersonallifetracker.adapters.expenseAdapter;
 import com.google.android.material.dialog.MaterialAlertDialogBuilder;
-//import com.google.firebase.database.DataSnapshot;
-//import com.google.firebase.database.DatabaseError;
-//import com.google.firebase.database.DatabaseReference;
-//import com.google.firebase.database.FirebaseDatabase;
-//import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 import java.text.DateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
 
-public class expenseFragment extends Fragment
-{
+import kotlin.random.Random;
+
+public class expenseFragment extends Fragment {
     private Button expenseButton,statusButton,addBalance;
     private TextView PreviousBalance,CurrentBalance,LastTransaction , date;
     private EditText balance;
@@ -49,6 +50,7 @@ public class expenseFragment extends Fragment
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
+
     }
 
     @Override
@@ -80,6 +82,19 @@ public class expenseFragment extends Fragment
         recyclerView.setHasFixedSize(true);
         expenseAdapter adapter = new expenseAdapter(mylist, getContext());
         recyclerView.setAdapter(adapter);
+//
+        ExpenseClass item1 = new ExpenseClass(100, "Snacks", "Snacks", "1");
+        mylist.add(item1);
+//        ExpenseClass item2 = new ExpenseClass(200, "Groceries", "Groceries", "2");
+//        mylist.add(item2);
+//        ExpenseClass item3 = new ExpenseClass(300, "Food", "Food", "3");
+//        mylist.add(item3);
+//        ExpenseClass item4 = new ExpenseClass(400, "Snacks", "Snacks", "4");
+//        mylist.add(item4);
+//        ExpenseClass item5 = new ExpenseClass(500, "Groceries", "Groceries", "5");
+//        mylist.add(item5);
+//        ExpenseClass item6 = new ExpenseClass(600, "Food", "Food", "6");
+//        mylist.add(item6);
 
 
         //getting database references
@@ -92,31 +107,35 @@ public class expenseFragment extends Fragment
         Calendar calendar = Calendar.getInstance();
         String currentDate = DateFormat.getDateInstance(DateFormat.FULL).format(calendar.getTime());
         date.setText(currentDate);
-        //addbalance from editText and send Mainbalance to database
 
-//        addBalance.setOnClickListener(new View.OnClickListener()
-//        {
-//            @Override
-//            public void onClick(View v)
-//            {
-//                String Balance = balance.getText().toString();
-//                BalanceClass balanceClass = new BalanceClass(Integer.parseInt(Balance), Integer.parseInt(Balance), 0);
-//                reference1.setValue(balanceClass);
-//            }
-//        });
+
+        String Balance = balance.getText().toString();
+        addBalance.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                CurrentBalance.setText(Balance);
+            }
+        });
+
 
 
         // initialize previous balance and currentbalance from database
-//        reference1.addValueEventListener(new ValueEventListener() {
+//        reference1.addValueEventListener(new ValueEventListener()
+//        {
 //            @Override
-//            public void onDataChange(@NonNull DataSnapshot snapshot) {
+//            public void onDataChange(@NonNull DataSnapshot snapshot)
+//            {
 //                BalanceClass balanceClass = snapshot.getValue(BalanceClass.class);
 //                PreviousBalance.setText(String.valueOf(balanceClass.getPreviousBalance()));
 //                CurrentBalance.setText(String.valueOf(balanceClass.getCurrentBalance()));
 //                LastTransaction.setText(String.valueOf(balanceClass.getLastTransaction()));
-//                addBalance.setOnClickListener(new View.OnClickListener() {
+//                addBalance.setOnClickListener(new View.OnClickListener()
+//                {
 //                    @Override
-//                    public void onClick(View v) {
+//                    public void onClick(View v)
+//                    {
 //                        String Balance =  balance.getText().toString();
 //                        String Currentbalance = Integer.parseInt(Balance) + Integer.parseInt(CurrentBalance.getText().toString()) + "";
 //                        BalanceClass balanceClass = new BalanceClass(Integer.parseInt(Currentbalance), Integer.parseInt(Currentbalance), Integer.parseInt("0"));
@@ -126,32 +145,37 @@ public class expenseFragment extends Fragment
 //            }
 //
 //            @Override
-//            public void onCancelled(@NonNull DatabaseError error) {
+//            public void onCancelled(@NonNull DatabaseError error)
+//            {
 //
 //            }
 //        });
-//
-//        //getting data from database and setting up recycler view with list
-//        reference.addValueEventListener(new ValueEventListener() {
+
+        //getting data from database and setting up recycler view with list
+//        reference.addValueEventListener(new ValueEventListener()
+//        {
 //            @Override
-//            public void onDataChange(@NonNull DataSnapshot snapshot) {
+//            public void onDataChange(@NonNull DataSnapshot snapshot)
+//            {
 //                mylist.clear();
-//                for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
+//                for (DataSnapshot dataSnapshot : snapshot.getChildren())
+//                {
 //                    ExpenseClass expenseClass = dataSnapshot.getValue(ExpenseClass.class);
 //                    mylist.add(expenseClass);
 //                }
 //                adapter.notifyDataSetChanged();
 //            }
-//
 //            @Override
-//            public void onCancelled(@NonNull DatabaseError error) {
+//            public void onCancelled(@NonNull DatabaseError error)
+//            {
 //
 //            }
 //        });
-        /// on swipe method
+//        /// on swipe method
 //        ItemTouchHelper.SimpleCallback simpleCallback = new ItemTouchHelper.SimpleCallback(0,ItemTouchHelper.LEFT|ItemTouchHelper.RIGHT) {
 //            @Override
-//            public boolean onMove(@NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, @NonNull RecyclerView.ViewHolder target) {
+//            public boolean onMove(@NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, @NonNull RecyclerView.ViewHolder target)
+//            {
 //                return false;
 //            }
 //
@@ -159,15 +183,49 @@ public class expenseFragment extends Fragment
 //            public void onSwiped(@NonNull RecyclerView.ViewHolder viewHolder, int direction) {
 //                int position = viewHolder.getAdapterPosition();
 //                ExpenseClass item = mylist.get(position);
-//                switch (direction){
+//                switch (direction)
+//                {
+//
 //                    case ItemTouchHelper.LEFT:
-//                        reference.child(item.getId()).removeValue();
-//                        adapter.notifyDataSetChanged();
+//                        reference1.addListenerForSingleValueEvent(new ValueEventListener()
+//                        {
+//                            @Override
+//                            public void onDataChange(@NonNull DataSnapshot snapshot) {
+//                                BalanceClass balanceClass = snapshot.getValue(BalanceClass.class);
+//                                int currentBalance = balanceClass.getCurrentBalance() + item.getAmount();
+//                                int last = balanceClass.getLastTransaction();
+//                                BalanceClass newbalanceClass = new BalanceClass(currentBalance, currentBalance, last);
+//                                reference1.setValue(newbalanceClass);
+//                                reference.child(item.getId()).removeValue();
+//                                adapter.notifyDataSetChanged();
+//                            }
+//
+//                            @Override
+//                            public void onCancelled(@NonNull DatabaseError error) {
+//
+//                            }
+//                        });
 //                        break;
 //
 //                    case ItemTouchHelper.RIGHT:
-//                        reference.child(item.getId()).removeValue();
-//                        adapter.notifyDataSetChanged();
+//                        reference1.addListenerForSingleValueEvent(new ValueEventListener()
+//                        {
+//                            @Override
+//                            public void onDataChange(@NonNull DataSnapshot snapshot) {
+//                                BalanceClass balanceClass = snapshot.getValue(BalanceClass.class);
+//                                int currentBalance = balanceClass.getCurrentBalance() + item.getAmount();
+//                                int last = balanceClass.getLastTransaction();
+//                                BalanceClass newbalanceClass = new BalanceClass(currentBalance, currentBalance, last);
+//                                reference1.setValue(newbalanceClass);
+//                                reference.child(item.getId()).removeValue();
+//                                adapter.notifyDataSetChanged();
+//                            }
+//
+//                            @Override
+//                            public void onCancelled(@NonNull DatabaseError error) {
+//
+//                            }
+//                        });
 //
 //                        break;
 //                }
@@ -182,9 +240,10 @@ public class expenseFragment extends Fragment
         //add button click and showing dialogue
         expenseButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                View view1 = LayoutInflater.from(getContext()).inflate(R.layout.expensedialogue, null);
-                Spinner spinner = view1.findViewById(R.id.spinner);
+            public void onClick(View v)
+            {
+                View expenseDialogue = LayoutInflater.from(getContext()).inflate(R.layout.expensedialogue, null);
+                Spinner spinner = expenseDialogue.findViewById(R.id.spinner);
                 ArrayList<String> arrayList = new ArrayList<>();
                 arrayList.add("Bills");
                 arrayList.add("Food");
@@ -192,44 +251,51 @@ public class expenseFragment extends Fragment
                 arrayList.add("Groceries");
                 arrayList.add("Hangout");
                 arrayList.add("Others");
+
+
                 ArrayAdapter<String> adapter = new ArrayAdapter<String>(getContext(), R.layout.spinnerview, R.id.spinnerText, arrayList);
                 spinner.setAdapter(adapter);
                 AlertDialog alertDialog = new MaterialAlertDialogBuilder(getContext())
-                        .setView(view1)
+                        .setView(expenseDialogue)
                         .setTitle("Add Expense")
-                        .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                        .setNegativeButton("Cancel", new DialogInterface.OnClickListener()
+                        {
                             @Override
                             public void onClick(DialogInterface dialog, int which) {
                                 //nothing
                             }
                         })
-                        .setPositiveButton("Add", new DialogInterface.OnClickListener() {
+                        .setPositiveButton("Add", new DialogInterface.OnClickListener()
+                        {
                             @Override
-                            public void onClick(DialogInterface dialog, int which) {
+                            public void onClick(DialogInterface dialog, int which)
+                            {
 
 
-                                EditText amount = view1.findViewById(R.id.amount);
-                                EditText description = view1.findViewById(R.id.details);
-                                Spinner spinner = view1.findViewById(R.id.spinner);
-
-
+                                EditText amount = expenseDialogue.findViewById(R.id.amount);
+                                EditText description = expenseDialogue.findViewById(R.id.details);
+                                Spinner spinner = expenseDialogue.findViewById(R.id.spinner);
                                 String type = spinner.getSelectedItem().toString();
                                 String amount1 = amount.getText().toString();
                                 String description1 = description.getText().toString();
-                                //String key = reference.push().getKey();
-//                                ExpenseClass expenseClass = new ExpenseClass(Integer.parseInt(amount1), description1, type,key);
+                                int key = 1 ;
+                                key++;
+                                String Key = String.valueOf(key);
+                                ExpenseClass expenseClass = new ExpenseClass(Integer.parseInt(amount1), description1, type,Key);
 
                                 String PB = PreviousBalance.getText().toString();
                                 String CB = CurrentBalance.getText().toString();
+                                mylist.add(expenseClass);
+                                adapter.notifyDataSetChanged();
 
 
                                 BalanceClass newbalanceClass = new BalanceClass(Integer.parseInt(CB), Integer.parseInt(CB) - Integer.parseInt(amount1), Integer.parseInt(amount1));
                                 //DatabaseReference reference1 = FirebaseDatabase.getInstance().getReference().child("Expense").child("MainBalance");
 
 
-//
-//                                reference1.setValue(newbalanceClass);
-//                                reference.child(key).setValue(expenseClass);
+                                //reference1.setValue(newbalanceClass);
+                                //reference.child(key).setValue(expenseClass);
+
                             }
                         })
                         .create();
@@ -238,5 +304,5 @@ public class expenseFragment extends Fragment
             }
         });
     }
-}
 
+}
